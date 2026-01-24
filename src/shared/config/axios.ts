12d4990b3a02ab68b38fde7bf7b7944e42f2d657
Facundo Robert <robert.facundo@hotmail.com>
2026-01-24 11:40:50 -0300

    vercel redirections

--- src/shared/config/axios.ts
+++ src/shared/config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '../redux/store';
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://musicappbackend-ccph.onrender.com/';
 
 export const api = axios.create({
     baseURL: API_URL,
